b2f613f5db2685c590c56ad6d43ae0f4c4c348ca
didac <dimolsan13@gmail.com>
2026-02-26 21:50:35 +0100

    Rename 'programming' to 'coding' in userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -158,7 +158,7 @@ const default_configuration = {
           ],
         },
         {
-          name: "programming",
+          name: "coding",
           links: [
             {
               name: "Jutge",
